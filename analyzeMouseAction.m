19aa9e124b65b095e5f2516321c33901e27e41d7
Shantanu Ray <chico.chego@gmail.com>
2017-02-15 16:31:14 +0800

    New Mode=background with no video.No outcome o/p

--- analyzeMouseAction.m
+++ analyzeMouseAction.m
@@ -1,5 +1,5 @@
-function [r,theta,diffXY,actionSpec,refCentroid,pawCentroid,traceVideoFile] = analyzeMouseAction(roiData, grabResult, videoFile, modeFlag)
-% [r,theta,diffXY,outcome,refCentroid,pawCentroid,traceVideoFile] = analyzeMouseAction(roiData, grabResult, videoFile, modeFlag);
+function [r,theta,diffXY,refCentroid,pawCentroid,traceVideoFile] = analyzeMouseAction(roiData, grabResult, videoFile, modeFlag)
+% [r,theta,diffXY,refCentroid,pawCentroid,traceVideoFile] = analyzeMouseAction(roiData, grabResult, videoFile, modeFlag);
 % -------------Outputs --------------
 %   - r             : Absolute distance of paw from pellet
 %   - theta         : Angle of approach of paw from pellet with respect to horizontal axis
@@ -11,10 +11,10 @@ function [r,theta,diffXY,actionSpec,refCentroid,pawCentroid,traceVideoFile] = an
 %   - pawCentroid   : Absolute paw center in original video
 %
 % -------------- Usage --------------
-% [r,theta,diffXY,outcome] = analyzeMouseAction;
+% [r,theta,diffXY] = analyzeMouseAction;
 % User will be asked to load a previously saved .mat file with the necessary inputs
 %
-% [r,theta,diffXY,outcome] = analyzeMouseAction(roiData, grabResult, videoFile, modeFlag);
+% [r,theta,diffXY] = analyzeMouseAction(roiData, grabResult, videoFile, modeFlag);
 %
 % -------------- Inputs --------------
 % Provide necessary inputs. Please maintain structure as follows
@@ -27,7 +27,9 @@ function [r,theta,diffXY,actionSpec,refCentroid,pawCentroid,traceVideoFile] = an
 %                       * Prehension (user suggested label for prehension)
 %                       Structure with fields ('outcome','position','centroid','imageFile','frameCount')
 %   - videoFile:        original video file location (string)
-%   - modeFlag:         Process in 'foreground' (show videos and plots) or in 'background' (only save)
+%   - modeFlag:         Process in 'foreground' (show videos and plots) or
+%   'background-video' (only save with videos) or 'background' (only data -
+%   no videos)
 %
 %%%%% See markMouseAction.m for further reference %%%%
 
@@ -44,41 +46,44 @@ if nargin<4
         modeFlag = 'foreground';
     end
 end
-% Video Sizes
-actualVidSize = [1080 1920 3];                                      % RGB image of 1080x1920
-displayResizeFactor = 2.5;                                          % For display, resize images
-displayVidSize = [ceil(actualVidSize(1:2)/displayResizeFactor), 3]; % RGB image of 720x1280
-% Size the atari box size (paw and pellet)
-boxSize = 5;
-% Init the pellet atari to green
-pelletBoxColor = zeros(boxSize*2+1,boxSize*2+1,3);
-pelletBoxColor(:,:,2) = 255; % Green
-% Init the paw atari to red
-pawBoxColor = zeros(boxSize*2+1,boxSize*2+1,3);
-pawBoxColor(:,:,1) = 255; % Red
-% Init the trace
-traceSize = 2;
-% Init the trace atari to yellow
-traceBoxColor = zeros(traceSize*2+1,traceSize*2+1,3);
-traceBoxColor(:,:,1:2) = 255; % Yellow
-frameRate = 4;
+videoFile = '';
+if strcmpi(modeFlag, 'background-video') | strcmpi(modeFlag, 'foreground')
+    % Video Sizes
+    actualVidSize = [1080 1920 3];                                      % RGB image of 1080x1920
+    displayResizeFactor = 2.5;                                          % For display, resize images
+    displayVidSize = [ceil(actualVidSize(1:2)/displayResizeFactor), 3]; % RGB image of 720x1280
+    % Size the atari box size (paw and pellet)
+    boxSize = 5;
+    % Init the pellet atari to green
+    pelletBoxColor = zeros(boxSize*2+1,boxSize*2+1,3);
+    pelletBoxColor(:,:,2) = 255; % Green
+    % Init the paw atari to red
+    pawBoxColor = zeros(boxSize*2+1,boxSize*2+1,3);
+    pawBoxColor(:,:,1) = 255; % Red
+    % Init the trace
+    traceSize = 2;
+    % Init the trace atari to yellow
+    traceBoxColor = zeros(traceSize*2+1,traceSize*2+1,3);
+    traceBoxColor(:,:,1:2) = 255; % Yellow
+    frameRate = 4;
 
-% Location of saved video
-[savedir, savePrefix]=fileparts(videoFile);
+    % Location of saved video
+    [savedir, savePrefix]=fileparts(videoFile);
 
-% Init the video reader
-videoReader = vision.VideoFileReader(videoFile);
+    % Init the video reader
+    videoReader = vision.VideoFileReader(videoFile);
 
-% Init the foreground detector (mask)
-maskDetector = vision.ForegroundDetector();     % Using default parameters
+    % Init the foreground detector (mask)
+    maskDetector = vision.ForegroundDetector();     % Using default parameters
 
-% Init video writers
-traceVideoFile{1}=fullfile(savedir, [savePrefix,'_Trace.mp4']);
-atariVideoWriter    = vision.VideoFileWriter(traceVideoFile{1}, 'FrameRate', frameRate, 'FileFormat', 'MPEG4');
-traceVideoFile{2}=fullfile(savedir, [savePrefix,'_VideoWTrace.mp4']);
-vwtVideoWriter     = vision.VideoFileWriter(traceVideoFile{2}, 'FrameRate', frameRate, 'FileFormat', 'MPEG4');
-traceVideoFile{3}=fullfile(savedir, [savePrefix,'_Mask.mp4']);
-maskVideoWriter     = vision.VideoFileWriter(traceVideoFile{3}, 'FrameRate', frameRate, 'FileFormat', 'MPEG4');
+    % Init video writers
+    traceVideoFile{1}=fullfile(savedir, [savePrefix,'_Trace.mp4']);
+    atariVideoWriter    = vision.VideoFileWriter(traceVideoFile{1}, 'FrameRate', frameRate, 'FileFormat', 'MPEG4');
+    traceVideoFile{2}=fullfile(savedir, [savePrefix,'_VideoWTrace.mp4']);
+    vwtVideoWriter     = vision.VideoFileWriter(traceVideoFile{2}, 'FrameRate', frameRate, 'FileFormat', 'MPEG4');
+    traceVideoFile{3}=fullfile(savedir, [savePrefix,'_Mask.mp4']);
+    maskVideoWriter     = vision.VideoFileWriter(traceVideoFile{3}, 'FrameRate', frameRate, 'FileFormat', 'MPEG4');
+end
 
 if strcmpi(modeFlag,'foreground')
     %% Init the video players
@@ -94,6 +99,7 @@ end
 
 
 %% Start processing
+
 % Get the centroid and bbox of the pellet
 [refCentroid, refBox] = getBox(roiData,'Pellet');
 
@@ -115,118 +121,120 @@ if strcmpi(modeFlag,'foreground')
     h = plotPawTrajectory(diffXY, r, theta);
 end
 
-%% Save video
-% Init the mask and the atari base images
-mask   = uint8(zeros(1080,1920,3));
-atari   = uint8(zeros(1080,1920,3));
-vwt     = uint8(zeros(1080,1920,3));
-bbox    = [];
-outcome = {};
-actionSpec = {};
-for i = 1:pawFrames(end)
-    % We are using the original video to superimpose to the grab
-    % We go through every frame of the original video
-    frame = videoReader.step();
+if strcmpi(modeFlag,'foreground') | strcmpi(modeFlag,'background-video')
+    %% Save video
+    % Init the mask and the atari base images
+    mask   = uint8(zeros(1080,1920,3));
+    atari   = uint8(zeros(1080,1920,3));
+    vwt     = uint8(zeros(1080,1920,3));
+    bbox    = [];
+    outcome = {};
+    actionSpec = {};
+    for i = 1:pawFrames(end)
+        % We are using the original video to superimpose to the grab
+        % We go through every frame of the original video
+        frame = videoReader.step();
 
-    %% Create the atari image
-    % Reset the image
-    atari = uint8(zeros(1080,1920,3));
-    % Write the pellet as green
-    atari(refCentroid(1,2)-boxSize:refCentroid(1,2)+boxSize,refCentroid(1,1)-boxSize:refCentroid(1,1)+boxSize,:)=pelletBoxColor;
-    
-    %% Create the video with trace image
-    % Reset the image
-    vwt = frame;
-    % Write the pellet as green
-    vwt(refCentroid(1,2)-boxSize:refCentroid(1,2)+boxSize,refCentroid(1,1)-boxSize:refCentroid(1,1)+boxSize,:)=pelletBoxColor;
-    
-    %% Create the mask image
-    % Reset the image
-    mask    = uint8(zeros(1080,1920,3));
-    % Detect mask foreground as binarized image (0,1)
-    maskBin = maskDetector.step(frame);
-    % Apply morphological operations to remove noise and fill in holes.
-    maskBin = imopen(maskBin, strel('rectangle', [3,3]));
-    % Save to mask image in rgb
-    [maskX,maskY]   = find(maskBin);
-    for k = 1:length(maskX)
-        mask(maskX(k),maskY(k),:)=255;  % TODO: Find a better way to do this
-    end
-    % Write the pellet as green
-    mask(refCentroid(1,2)-boxSize:refCentroid(1,2)+boxSize,refCentroid(1,1)-boxSize:refCentroid(1,1)+boxSize,:)=pelletBoxColor;
-    
-    %% Mark the paw in the video
-    % But the marking may not have been done on every frame
-    % So we add the paw only if the frame has been marked
-    loc=find(i>=pawFrames); % Get the index of the saved markings uptil current frame
-    
-    if ~isempty(loc) & ~isempty(pawCentroid(loc(end))) 
-        %% NOTE: Additional Checking For ~isempty(pawCentroid(loc(end)))
-        % This is because at times marking the paw may not have worked and getBox returns empty
-        % See sub-function imageMark in markMouseAction where check for length(centroids)~=2
+        %% Create the atari image
+        % Reset the image
+        atari = uint8(zeros(1080,1920,3));
+        % Write the pellet as green
+        atari(refCentroid(1,2)-boxSize:refCentroid(1,2)+boxSize,refCentroid(1,1)-boxSize:refCentroid(1,1)+boxSize,:)=pelletBoxColor;
 
-        % Get the latest frame wrt index of the saved markings
-        % This is  index 'i' if marking has been done for 'i',
-        % else it is the previous frame where marking has been done
-        curidx = loc(end); 
-        % Write the paw as red
-        atari(pawCentroid(curidx,2)-boxSize:pawCentroid(curidx,2)+boxSize,pawCentroid(curidx,1)-boxSize:pawCentroid(curidx,1)+boxSize,:)=pawBoxColor;
-        % Write the paw as red
-        vwt(pawCentroid(curidx,2)-boxSize:pawCentroid(curidx,2)+boxSize,pawCentroid(curidx,1)-boxSize:pawCentroid(curidx,1)+boxSize,:)=pawBoxColor;
-        % Mark trajectory as yellow
-        for j = loc
-            vwt(pawCentroid(j,2)-2:pawCentroid(j,2)+2,pawCentroid(j,1)-2:pawCentroid(j,1)+2,:)=traceBoxColor;
+        %% Create the video with trace image
+        % Reset the image
+        vwt = frame;
+        % Write the pellet as green
+        vwt(refCentroid(1,2)-boxSize:refCentroid(1,2)+boxSize,refCentroid(1,1)-boxSize:refCentroid(1,1)+boxSize,:)=pelletBoxColor;
+
+        %% Create the mask image
+        % Reset the image
+        mask    = uint8(zeros(1080,1920,3));
+        % Detect mask foreground as binarized image (0,1)
+        maskBin = maskDetector.step(frame);
+        % Apply morphological operations to remove noise and fill in holes.
+        maskBin = imopen(maskBin, strel('rectangle', [3,3]));
+        % Save to mask image in rgb
+        [maskX,maskY]   = find(maskBin);
+        for k = 1:length(maskX)
+            mask(maskX(k),maskY(k),:)=255;  % TODO: Find a better way to do this
         end
+        % Write the pellet as green
+        mask(refCentroid(1,2)-boxSize:refCentroid(1,2)+boxSize,refCentroid(1,1)-boxSize:refCentroid(1,1)+boxSize,:)=pelletBoxColor;
 
-        % Mark the paw in the mask
-        % Write the paw as red
-        mask(pawCentroid(curidx,2)-boxSize:pawCentroid(curidx,2)+boxSize,pawCentroid(curidx,1)-boxSize:pawCentroid(curidx,1)+boxSize,:)=pawBoxColor;
-        if isempty(grabResult)
-            match   = [];
-        else
-            match = find(i ==[grabResult(:).frameCount]);
-        end
-        if ~isempty(match)
-            % if there is a coinciding marked action (reach, grasp, retrieve), then mark the action & action outcome
-            % If there are multiple actions marked for the same frame, in the video show only the first one marking
-%             outcome = [outcome;{[int2str(match(1)),': ',grabResult(match(1)).action,'-',grabResult(match(1)).actionType]}];
-%             bbox    = [bbox;[grabResult(match(1)).position]];
-            outcome = [{[int2str(match(1)),': ',grabResult(match(1)).action,'-',grabResult(match(1)).actionType]}];
-            bbox    = [[grabResult(match(1)).position]];
+        %% Mark the paw in the video
+        % But the marking may not have been done on every frame
+        % So we add the paw only if the frame has been marked
+        loc=find(i>=pawFrames); % Get the index of the saved markings uptil current frame
 
-            % If there are multiple actions marked for the same frame, save all actions
-            actionCell = struct2cell(grabResult(match));
-            % Only need to save the first 3 elements of the cell
-            % Note: Strucure is important. Assumption action, actionType,
-            % consequence are the first 3 elements of the structure
-            % TODO: Remove dependence on fixed structure
-            actionCell = actionCell(1:3,:);
-            actionSpec{curidx,1} = actionCell;
-         else
-            actionSpec{curidx,1} = {};
+        if ~isempty(loc) & ~isempty(pawCentroid(loc(end))) 
+            %% NOTE: Additional Checking For ~isempty(pawCentroid(loc(end)))
+            % This is because at times marking the paw may not have worked and getBox returns empty
+            % See sub-function imageMark in markMouseAction where check for length(centroids)~=2
+
+            % Get the latest frame wrt index of the saved markings
+            % This is  index 'i' if marking has been done for 'i',
+            % else it is the previous frame where marking has been done
+            curidx = loc(end); 
+            % Write the paw as red
+            atari(pawCentroid(curidx,2)-boxSize:pawCentroid(curidx,2)+boxSize,pawCentroid(curidx,1)-boxSize:pawCentroid(curidx,1)+boxSize,:)=pawBoxColor;
+            % Write the paw as red
+            vwt(pawCentroid(curidx,2)-boxSize:pawCentroid(curidx,2)+boxSize,pawCentroid(curidx,1)-boxSize:pawCentroid(curidx,1)+boxSize,:)=pawBoxColor;
+            % Mark trajectory as yellow
+            for j = loc
+                vwt(pawCentroid(j,2)-2:pawCentroid(j,2)+2,pawCentroid(j,1)-2:pawCentroid(j,1)+2,:)=traceBoxColor;
+            end
+
+            % Mark the paw in the mask
+            % Write the paw as red
+            mask(pawCentroid(curidx,2)-boxSize:pawCentroid(curidx,2)+boxSize,pawCentroid(curidx,1)-boxSize:pawCentroid(curidx,1)+boxSize,:)=pawBoxColor;
+            if isempty(grabResult)
+                match   = [];
+            else
+                match = find(i ==[grabResult(:).frameCount]);
+            end
+            if ~isempty(match)
+                % if there is a coinciding marked action (reach, grasp, retrieve), then mark the action & action outcome
+                % If there are multiple actions marked for the same frame, in the video show only the first one marking
+                % outcome = [outcome;{[int2str(match(1)),': ',grabResult(match(1)).action,'-',grabResult(match(1)).actionType]}];
+                % bbox    = [bbox;[grabResult(match(1)).position]];
+                outcome = [{[int2str(match(1)),': ',grabResult(match(1)).action,'-',grabResult(match(1)).actionType]}];
+                bbox    = [[grabResult(match(1)).position]];
+
+             %    % If there are multiple actions marked for the same frame, save all actions
+             %    actionCell = struct2cell(grabResult(match));
+             %    % Only need to save the first 3 elements of the cell
+             %    % Note: Strucure is important. Assumption action, actionType,
+             %    % consequence are the first 3 elements of the structure
+             %    % TODO: Remove dependence on fixed structure
+             %    actionCell = actionCell(1:3,:);
+             %    actionSpec{curidx,1} = actionCell;
+             % else
+             %    actionSpec{curidx,1} = {};
+            end
         end
+        if ~isempty(bbox)
+            % Mark the actions on the videos
+            atari   = insertObjectAnnotation(atari, 'rectangle', bbox, outcome);
+            mask    = insertObjectAnnotation(mask, 'rectangle', bbox, outcome);
+            vwt     = insertObjectAnnotation(vwt, 'rectangle', bbox, outcome);
+        end
+        if strcmpi(modeFlag,'foreground')
+            atariPlayer.step(imresize(atari,1/displayResizeFactor));
+            vwtPlayer.step(imresize(vwt,1/displayResizeFactor));
+            maskPlayer.step(imresize(mask,1/displayResizeFactor));
+            pause(1/frameRate);
+        end
+        step(atariVideoWriter, atari);
+        step(maskVideoWriter, mask);
+        step(vwtVideoWriter, vwt);
+
     end
-    if ~isempty(bbox)
-        % Mark the actions on the videos
-        atari   = insertObjectAnnotation(atari, 'rectangle', bbox, outcome);
-        mask    = insertObjectAnnotation(mask, 'rectangle', bbox, outcome);
-        vwt     = insertObjectAnnotation(vwt, 'rectangle', bbox, outcome);
-    end
-    if strcmpi(modeFlag,'foreground')
-        atariPlayer.step(imresize(atari,1/displayResizeFactor));
-        vwtPlayer.step(imresize(vwt,1/displayResizeFactor));
-        maskPlayer.step(imresize(mask,1/displayResizeFactor));
-        pause(1/frameRate);
-    end
-    step(atariVideoWriter, atari);
-    step(maskVideoWriter, mask);
-    step(vwtVideoWriter, vwt);
-    
+    release(atariVideoWriter);
+    release(maskVideoWriter);
+    release(vwtVideoWriter);
+    videoFile=traceVideoFile;
 end
-release(atariVideoWriter);
-release(maskVideoWriter);
-release(vwtVideoWriter);
-videoFile=traceVideoFile;
 
     %% For the given box, [x y width height], return the selected image with actual
     % coordinates [row(1):row(end), column(1):column(end)]
